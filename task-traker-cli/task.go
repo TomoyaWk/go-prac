cfe0df27f8a9f75f8493b8895557dd9ee73ce11e
TomoyaWk <wakatomo0606@gmail.com>
2024-12-27 15:25:22 +0900

    expense-tracker init

--- task-traker-cli/task.go
+++ task-traker-cli/task.go
@@ -64,7 +64,7 @@ func ParseStatus(s string) (Status, error) {
 type Task struct {
 	Id          int       `json:"id"`
 	Description string    `json:"description"`
-	Status      string    `json:status.ParseStatus()`
+	Status      string    `json:"status"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
 }
